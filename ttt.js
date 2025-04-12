c5a610a82b93e22e32654a5adef71478fd44fefe
ProjectNerva <hedge.heisenberg@gmail.com>
2025-04-11 22:47:56 -0700

    TTT Change-Up

--- ttt.js
+++ ttt.js
@@ -1,11 +1,13 @@
-const board = document.getElementById('board')
-const squares = document.getElementsByClassName('square')
-const players = ['X', 'O']
-let currentPlayer = players[0]
-const endMessage = document.createElement('h2')
-endMessage.textContent = `X's turn!`
-endMessage.style.marginTop = '30px'
-endMessage.style.textAlign='center'
+// followed the tutorial https://medium.com/@canankorkut1/how-to-create-a-tic-tac-toe-with-html-css-and-javascript-10a25fddd356
+
+const board = document.getElementById('board');
+const squares = document.getElementsByClassName('square');
+const players = ['X', 'O'];
+let currentPlayer = players[0];
+const endMessage = document.createElement('h2');
+endMessage.textContent = "X's turn!";
+endMessage.style.marginTop = '30px';
+endMessage.style.textAlign='center';
 board.after(endMessage)
 
 const winning_combinations = [
@@ -19,37 +21,45 @@ const winning_combinations = [
     [2, 4, 6]
 ]
 
+// main game loop
 for(let i = 0; i < squares.length; i++){
+    // triggers when a box is pressed
     squares[i].addEventListener('click', () => {
+        // fill the empty square
         if(squares[i].textContent !== ''){
-            return
+            return;
         }
+
+        //check win condition
         squares[i].textContent = currentPlayer
         if(checkWin(currentPlayer)) {
-            endMessage.textContent=`Game over! ${currentPlayer} wins!`
-            return
+            endMessage.textContent=`Game over! ${currentPlayer} wins!`;
+            return;
         }
         if(checkTie()) {
-            endMessage.textContent= `Game is tied!`
-            return
+            endMessage.textContent= 'Game is tied!';
+            return;
         }
-        currentPlayer = (currentPlayer === players[0]) ? players[1] : players[0] 
+
+        // goes to next player
+        currentPlayer = (currentPlayer == players[0]) ? players[1] : players[0] 
         if(currentPlayer == players[0]) {
-            endMessage.textContent= `X's turn!`
+            endMessage.textContent= "X's turn!";
         } else {
-            endMessage.textContent= `O's turn!`
+            endMessage.textContent= "O's turn!";
         }     
     })   
 }
 
 function checkWin(currentPlayer) {
     for(let i = 0; i < winning_combinations.length; i++){
-        const [a, b, c] = winning_combinations[i]
-        if(squares[a].textContent === currentPlayer && squares[b].textContent === currentPlayer && squares[c].textContent === currentPlayer){
-            return true
+        const [a, b, c] = winning_combinations[i]; // retrieving the combiation
+        // checks the square
+        if(squares[a].textContent == currentPlayer && squares[b].textContent == currentPlayer && squares[c].textContent == currentPlayer){
+            return true;
         }
     }
-    return false
+    return false;
 }
 
 function checkTie(){
@@ -58,13 +68,13 @@ function checkTie(){
             return false;
         }
     }
-    return true
+    return true;
 }
 
 function restartButton() {
     for(let i = 0; i < squares.length; i++) {
-        squares[i].textContent = ""
+        squares[i].textContent = "";
     }
-    endMessage.textContent=`X's turn!`
-    currentPlayer = players[0]
+    endMessage.textContent=`X's turn!`;
+    currentPlayer = players[0];
 }
